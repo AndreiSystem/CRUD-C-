0bec513c3abbf8633acfae840c1616f9a0ca250d
Andrei Gustavo <andrei.gustavo96@gmail.com>
2020-11-08 23:01:52 -0300

    Criando Parte de Create Students no mongo mais estrutura definida pelo framework

---
+++ CrudByAndrei/CrudByAndrei/Models/STudent.cs
@@ -0,0 +1,16 @@
+﻿using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CrudByAndrei.Models
+{
+    public class Student
+    {
+        public ObjectId _id { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string emailAddress { get; set; }
+    }
+}
